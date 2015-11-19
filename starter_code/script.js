3684c7913d0f6b39733503b3267ca218b41c9648
pespina <pespinal@cristoreyny.org>
2015-11-19 13:17:32 +0000

    hai

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,23 +1,57 @@
-var userPoint = 0;
-var aiPoint = 0;
+    var userPoint = 0;
+    var aiPoint = 0;
 
 // This function returns the selection of the computer
+
 function getAISelection() {
-    //TODO: randomly choose between 'rock', 'paper', or 'scissors'
+    
+//TODO: randomly choose between 'rock', 'paper', or 'scissors'
+    
+    var options = ["rock","paper","scissors"];
+    var pickOption = Math.floor(Math.random()*3);
+        console.log(pickOption);
+        return options[pickOption];
+   
 }
+
+    
+
 
 // This function picks the winner
 function pickWinner(userValue, aiValue) {
-    //TODO: pick the correct winner: user or ai
-    //TODO: Add one point for the winner
-}
+    if (userValue == aiValue) {
+        return "draw";
+        } 
+    if (userValue == "rock" && aiValue == "paper") {
+        return "ai" ;
+        }
+    if (userValue== "rock" && aiValue == "scissors") {
+        return "user";
+        }
+    if (userValue == "paper" && aiValue == "rock") {
+        return "user";
+        }
+    if (userValue == "paper" && aiValue == "scissors") {
+        return "ai";
+        }
+    if (userValue == "scissors" && aiValue == "rock") {
+        return "ai"}
+    if (userValue == "scissors" && aiValue == "paper") {
+        return "user";
+        }
+    }
+    
 
 // This function sets the scoreboard with the correct points
-function setScore() {
 
+   
+function setScore() {
+        $("#userPoint").text(userPoint);
+        $("#aiPoint").text(aiPoint);
 }
 
 // This function captures the click and picks the winner
+
 function evaluate(evt) {
     var userValue = evt.target.getAttribute('id');
     var aiValue = getAISelection();
@@ -25,15 +59,25 @@ function evaluate(evt) {
     var winner = pickWinner(userValue, aiValue);
 
     if ( 'user' === winner ) {
+        userPoint++;
         $('#message').delay(50).text('You have won!, Click a box to play again');
     } else if ( winner === 'draw' ) {
         $('#message').delay(50).text('Draw! Click a box to play again');
     } else {
+        aiPoint++;
         $('#message').delay(50).text('You have lost!, Click a box to play again');
     }
+     setScore();
 }
 
 // This function runs on page load
-$(document).ready(function(){
 
-});
+$(document).ready(function(){
+    $(".token").click(evaluate);
+   
+       
+});     
+
+
+
+//bruhhhh the choose winner thing isnt working and neither is the 
